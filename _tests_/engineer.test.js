d6840779ac0542cb43872c3175b35528b0f8b379
solo <solohuckstep@gmail.com>
2022-11-08 00:41:08 -0500

    added engineer test

---
+++ _tests_/engineer.test.js
@@ -0,0 +1,21 @@
+const engineer = require("../lib/engineer");
+
+test('getRole() return \"engineer\"', () => {
+    const testVal = "engineer";
+    const e = new engineer("Foo", 1, "testVal@testing.com", "GitHubUser");
+    expect(e.getRole()).toBe(testVal);
+});
+
+test('set GitHUb account with constructor', () => {
+    const testVal = "GitHubUser";
+    const e = new engineer("Foo", 1, "testVal@testing.com", testVal);
+    expect(e.github).toBe(testVal);
+});
+
+
+test("get the GitHub username with getGithub()", () => {
+    const testVal = "GitHubUser";
+    const e = new engineer("Foo", 1, "testVal@testing.com", testVal);
+    expect(e.getGithub()).toBe(testVal);
+});
+  
